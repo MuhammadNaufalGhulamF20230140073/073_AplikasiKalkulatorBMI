e7fb7401ce0b1443d07ed32235575c138f52dc3a
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2026-01-14 01:36:44 +0700

    menambahkan plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
 }
+
 
 android {
     namespace = "com.example.roomdb_kalkulatorbmi"

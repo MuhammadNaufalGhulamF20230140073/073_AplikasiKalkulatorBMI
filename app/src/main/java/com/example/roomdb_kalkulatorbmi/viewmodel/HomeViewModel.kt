ef85a712f0f42a9f147a3b31b0f6a53b9321f46d
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2026-01-14 22:56:40 +0700

    menambahkan StateFlow userList dari database ke HomeViewModel

--- app/src/main/java/com/example/roomdb_kalkulatorbmi/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/roomdb_kalkulatorbmi/viewmodel/HomeViewModel.kt
@@ -21,3 +21,18 @@ class HomeViewModel(
     // Variabel ini adalah pintu utama untuk mengelola Tabel User.
     private val repository: RepositoryUser
 ) : ViewModel() {
+
+    ///////////////////////////////////////////////////////////////////////////
+    // 2. DATA STREAM (Aliran Data Real-Time dari Database ke UI)
+    ///////////////////////////////////////////////////////////////////////////
+
+    // [userList] -> Didapat dari repository.getAllUser() (Query: SELECT * FROM user).
+    // Menggunakan stateIn agar data dari database (Flow) dikonversi menjadi StateFlow
+    // yang bisa dibaca oleh UI Compose secara efisien.
+    val userList = repository.getAllUser()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000), // Tetap aktif 5 detik meski layar mati
+            initialValue = emptyList() // Data awal kosong sebelum database merespon
+        )
+}
